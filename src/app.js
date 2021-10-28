af4c9726e3ef0f1cf1a850b839ba14052791276a
MargotMetzemaekers <92753046+MargotMetzemaekers@users.noreply.github.com>
2021-10-28 21:07:55 +0200

    Added function getCurrentLocation + eventlistener for button

--- src/app.js
+++ src/app.js
@@ -47,3 +47,9 @@ function searchLocation(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
