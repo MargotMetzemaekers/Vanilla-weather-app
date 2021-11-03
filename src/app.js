c19b372b28253a51a494cce001ab7f8517ffd5ef
MargotMetzemaekers <92753046+MargotMetzemaekers@users.noreply.github.com>
2021-11-03 18:45:51 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -99,3 +99,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+displayForecast();
