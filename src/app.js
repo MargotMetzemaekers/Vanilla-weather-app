a69b77f652d3fafc5a18062e3ba72acf6ad3ab6b
MargotMetzemaekers <92753046+MargotMetzemaekers@users.noreply.github.com>
2021-11-03 19:13:35 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,21 +16,30 @@ function formatDate(date) {
 function displayForecast() {
   let forecastElement = document.querySelector("#weather-forecast");
 
-  forecastElement.innerHTML = `
-  
-            <div class="row">
-              <div class="col-2">
-                <div class="weather-forecast-date">Friday</div>
-                  <img
-                  src="http://openweathermap.org/img/wn/50d@2x.png"
-                  alt=""
-                  width="42"/>
-                <div class="weather-forecast-temperatures">
-                  <span class="weather-forecast-temperature-max"> 18° </span>
-                  <span class="weather-forecast-temperature-min"> 12° </span>
-                </div>
-              </div>
-            </div>`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 let dateElement = document.querySelector("#date");
