360d1b815dbe7021be7d144dd9f05bbc0fe01d7f
MargotMetzemaekers <92753046+MargotMetzemaekers@users.noreply.github.com>
2021-11-04 19:20:13 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,24 +13,33 @@ function formatDate(date) {
   return `${days[dayIndex]} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#weather-forecast");
 
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (forecastDay) {
+  forecast.forEach(function (forecastDay, index) {
+    if (index <6) {
     forecastHTML =
       forecastHTML +
       `<div class="col-2">
-        <div class="weather-forecast-date">${forecastDay.dt}</div>
-        <img
-          src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+      <img
+          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${forecastDay.temp.max}° </span>
-          <span class="weather-forecast-temperature-min"> ${forecastDay.temp.min}° </span>
+          <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
         </div>
       </div>
   `;
@@ -38,6 +47,12 @@ function displayForecast(response) {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "cb286bad3607984b41ed10c8de5cf00e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 let dateElement = document.querySelector("#date");
