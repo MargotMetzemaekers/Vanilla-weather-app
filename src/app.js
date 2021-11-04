7d3bd62d816e26b12f2fd2f2a81e3aa098c4fb82
MargotMetzemaekers <92753046+MargotMetzemaekers@users.noreply.github.com>
2021-11-04 19:27:06 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-function formatDate(date) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -27,22 +28,28 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index <6) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
       <img
-          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
-          <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
         </div>
-      </div>
-  `;
+      </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
